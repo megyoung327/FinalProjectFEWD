5fb6a0c4d22b387d8db8bb34ea2551aa385ec557
Meg <megyoung327@gmail.com>
2016-04-14 17:00:57 -0400

    changes-made-2

--- index.js
+++ index.js
@@ -23,7 +23,6 @@ function changeBackgroundImage(event) {
     $('#content_second').html('<p>' + text2 + '</p>');
     $('#content_third').html('<p>' + text3 + '</p>');
 
-
 }
 
 $(document).ready(function() {
@@ -34,73 +33,11 @@ $(document).ready(function() {
 
     // define toggle menu function. 
     function toggleMenu() {
-      // first part of jquery is any css selector
-      $('nav ul').slideToggle();
+     $('nav ul').slideToggle();
 
 }
 
 });
 
         
-
-
-
-
-
-
-
-
-
-
-
-
-
-        //prevent default
-        //get the value that is selected in the drop down
-        //store that value in a variable
-
-        //then, based on the selected option, add and remove classes
-        //to the $('.demo')
-
-        //$('.demo').removeClass('default other-class something').addCoass('the-class-i-want')
-
-// When option is selected the assigned class will populate an image with
-// the superfood.
-        // if (option === 'Hydrating') {
-  //        console.log('Hydrating');
-  //        $('.demo').attr('cucumber');
-  //           $('#content').html('cucumber')
-        // } else if (option === 'Brightening') {
-  //        console.log('Brightening');
-  //        $('.demo').attr('spirulina');
-  //           $('#content').html('spinach')
-  //        // this.reset();
-  //    } else if (option === 'Benefiting mood and sleep') {
-  //        console.log('Benefiting mood and sleep');
-  //        $('.demo').attr('spinach');
-  //           $('#content').html('spinach')
-  //        // this.reset();
-  //    } else if (option === 'Detoxifying') {
-  //        console.log('Detoxifying');
-  //        $('.demo').attr('fennel');
-  //           $('#content').html('fennel')
-  //        // this.reset();
-  //    // } else if (option === '') {
-  //    //  console.log('Acne preventing');
-  //    //  $('.demo').attr('pumpkin-seeds');
-  //    //        $('#content').html('pumpkin-seeds')
-  //        // this.reset();
-  //    } else if (option === 'Skin softening') {
-  //        console.log('Skin softening');
-  //        $('.demo').attr('avocado');
-  //           $('#content').html('avocado')
-
-  //        // this.reset();
-  //    } else if (cityName === 'Anti-aging') {
-  //        console.log('Anti-aging');
-  //        $('.demo').attr('grapefruit');
-  //           $('#content').html('grapefruit');
-  //        // this.reset();
-  //    }
-
 
